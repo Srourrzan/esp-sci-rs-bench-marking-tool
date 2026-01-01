5662264641382e7817be971215eeea32ae1c8f52
Srourrzan <razanmsrour@gmail.com>
2026-01-01 21:56:18 +0300

    change data deconding to int8

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
+import struct;
+from sys import exit, stderr;
 from serial.tools import list_ports;
 from serial import Serial, SerialException;
-from sys import exit, stderr;
 
 from serial_port import find_port;
 
 def main():
     usb_port: str;
     status: int;
+    
     status, usb_port = find_port();
     if (status < 0):
         exit(1);
-    ser: Serial = Serial(usb_port, 921600);
     try:
-        while(1):
-            response: str = ser.readline();
-            print(f"response {type(response)}=\n{response.decode("utf-8").strip()}");
+        with Serial(usb_port, 921600) as ser:
+            while (True):
+                response: bytes = ser.readline();
+                values: int8 = struct.unpack(f"{len(response)}B", response);
+                print(f"values: {values}");
     except SerialException as e:
         print(f"{e}", file=stderr);
+    except PermissionError:
+        print("Permission denied - check user permissions", file=stderr);
+    except UnicodeDecodeError as e:
+        print(f"error while decoding: {e}", file=stderr);
+    except KeyboardInterrupt:
+        exit(127);
+    except Exception as e:
+        print(f"Unexpected error: {e}", file=stderr);
+    finally:
+        print(f"closing program");
+        exit(2);
     return ;
 
 if __name__ == "__main__":
--- serial_port.py
+++ serial_port.py
@@ -3,13 +3,15 @@ from sys import stderr;
 
 def find_port() -> (int, str):
     ports: list = list_ports.comports();
+    port: str = None;
     
     for port_ in ports:
-        if ((port_.description != "n/a") and (("/dev/ttyUSB" in port_.device) or ("COM" in port_.device))):
+        if ((port_.description != "n/a") and ("/dev/ttyUSB" in port_.device or "COM" in port_.device)):
             print(f"{port_.device} is found");
             port = port_.device;
-    if (port == None):
+            break;
+    if port is None:
         stderr.write("USB port not found\n");
-        return (-1, None);
+        return (-1, port);
     return (0, port);
 
