1218c3b0865252c0fdcd6ce0b531dabdcba07ec7
Srourrzan <razanmsrour@gmail.com>
2026-02-03 01:09:18 +0300

    Caclculate first version of latency and refactor: enhance config loading and improve serial communication handling

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-import struct;
+from csv import reader;
 from sys import exit, stderr;
+from typing import List, Dict;
+from json import load, JSONDecodeError;
 from serial import Serial, SerialException;
-from time import perf_counter;
-from json import load as jsload;
-from json import JSONDecodeError;
+from time import perf_counter, time_ns, sleep;
+from statistics import median, quantiles, stdev;
 
-from serial_port import find_port;
+from serial_port import find_port, init_serial;
 
-def load_config() -> (int, dict|None):
+def load_config() -> (int, Dict|None):
 	try:
 		with open("config.json", "r", encoding="utf-8") as file:
-			config = jsload(file)
+			config: Dict = load(file)
 	except JSONDecodeError as e:
 		print(f"Failed to decode JSON: {e}", file=stderr)
 		return (-1, None);
@@ -18,59 +19,110 @@ def load_config() -> (int, dict|None):
 		print(f"Config.json file was not found: {e}", file=stderr)
 		return (-1, None);
 	return (0, config);
-    
-def main():
-	"""
-	The timestamp coming from the esp-csi firmware, is coming from the Wi-Fi MAC hardware with microseconds resolution.
-	It is being assigned in the Wi-Fi driver at the moment a packet is received by the Wi-Fi hardwarer (PHY+MAC)
-	before it's passed up the stack.
 
-	To record host_recieve_ts, I used `time.perf_counter()` where it can measure in nanoseconds, always increasing, does not go backwards.
-
-	NEXT: I will calculate the clock offset via exchange protocol.
+def now_epoch_us() -> int:
 	"""
-	usb_port: str;
-	status: int;
-	configs: dict;
+	:return: Returns current epoch time in microseconds (int64).
+	:rtype: int
+	"""
+	return (time_ns() // 1000);
+
+def print_stats(deltas: List) -> None:
+	print("\nFinal stats:")
+	print(f" Median latency:	{median(deltas):.0f} micro second")
+	print(f" Std dev: 				{stdev(deltas):.0f} micro second")
+	print(f" Min / Max: 			{min(deltas)} / {max(deltas)} micro seconds")
+
+def validate_sys() -> (int, str|None, Dict|None):
+	"""
+	Docstring for validate_sys
 	
-	status, usb_port = find_port();
+	:return: Description
+	:rtype: Any
+	"""
+	usb_port: str
+	configs: Dict
+
+	status, usb_port = find_port()
 	if (status < 0):
-		exit(1);
+		return (-1, None, None);
 	status, configs = load_config()
 	if (status < 0):
-		exit(2);
+		return (-1, None, None);
+	return (0, usb_port, configs);
+
+def main() -> int:
+	"""
+	"""
+	status: int
+	usb_port: str
+	configs: dict
+	deltas: List[int] = []
+	col_index: Dict[str, int] = {}
+	
+	status, usb_port, configs = validate_sys()
+	if (status < 0):
+		return(1);
 	BAUD_RATE = configs["baud_rate"]
-	while(True):
-		try:
-			with Serial(usb_port, BAUD_RATE) as ser:
-				response: bytes = ser.readline();
+	try:
+		with Serial(usb_port, BAUD_RATE, timeout=10) as ser: #make timeout value part of json policy
+			init_serial(ser)
+			while (True):
+				response: bytes = ser.readline()
+				host_rx_epoch_us = now_epoch_us()
 				if not response:
+					print(f"no response", file=stderr)
 					continue;
-				host_receive_ts: float = perf_counter();
-				print(f"receive_ts = {host_receive_ts}");
-				line: str = response.decode("ascii", errors="ignore");
-				fields: list = line.strip().split(',');
-				print(f"fields =  {fields}");
-				timestamp: int = int(fields[18]);
-				print(f"timestamp: {timestamp}");
-				esp_epoch_us: int = int(fields[19]);
-				print(f"esp_epoch_us: {esp_epoch_us}")
-				# break;
-		except SerialException as e:
-			print(f"{e}", file=stderr);
-		except PermissionError:
-			print("Permission denied - check user permissions", file=stderr);
-		except UnicodeDecodeError as e:
-			print(f"error while decoding: {e}", file=stderr);
-		except KeyboardInterrupt:
-			exit(127);
-		except Exception as e:
-			print(f"Unexpected error: {e}", file=stderr);
-	return ;
+				line: str = response.decode("utf-8", errors="strict")
+				if not line:
+					print(f"failed to decode line", file=stderr)
+				line: str = line.strip()
+				if line.startswith("type,"):
+					reader_ = reader([line])
+					header: List = next(reader_)
+					col_index = {name: i for i, name in enumerate(header)}
+					if "esp_epoch_us" not in col_index:
+						print("ERROR: Header missing required field esp_epoch_us", file=stderr)
+						return (10);
+					continue
+				if not line.startswith("CSI_DATA"):
+					continue
+				reader_ = reader([line])
+				fields: List = next(reader_)
+				try:
+					esp_epoch_us = int(fields[col_index["esp_epoch_us"]])
+				except (ValueError, IndexError) as e:
+					print(f"Parse error: {e} | line: {line[:120]}")
+					continue
+				delta_us: int = host_rx_epoch_us - esp_epoch_us #vaive latency
+				deltas.append(delta_us)
+				if len(deltas) % 500 == 0:
+					med = median(deltas)
+					p90 = int(quantiles(deltas, n=10)[8])
+					p99 = int(quantiles(deltas, n=100)[98])
+					print(
+							f"N={len(deltas):>6}  "
+							f"median={med:>8.0f}μs  "
+							f"p90={p90:>8}μs  "
+							f"p99={p99:>8}μs  "
+							f"last={delta_us:>8}μs"
+					)
+	except SerialException as e:
+		print(f"{e}", file=stderr);
+	except PermissionError:
+		print("Permission denied - check user permissions", file=stderr);
+	except UnicodeDecodeError as e:
+		print(f"error while decoding: {e}", file=stderr);
+	except KeyboardInterrupt:
+		print("\nStopped by user.")
+		exit(127);
+	except Exception as e:
+		print(f"Unexpected error: {e}", file=stderr);
+	finally:
+		if deltas:
+			print_stats(deltas)
+	return (0);
 
 if __name__ == "__main__":
-	try:
-		main();
-	except SystemExit:
-		print("exiting program");
-	exit(0);
+	status = main();
+	exit(status);
--- serial_port.py
+++ serial_port.py
@@ -1,10 +1,13 @@
-from serial.tools import list_ports;
 from sys import stderr;
+from time import sleep;
+from serial import Serial;
+from serial.tools import list_ports;
 
 def find_port() -> (int, str):
-    ports: list = list_ports.comports();
+    ports: list;
     port: str = None;
     
+    ports = list_ports.comports();
     for port_ in ports:
         if ((port_.description != "n/a") and ("/dev/ttyUSB" in port_.device or "COM" in port_.device)):
             print(f"{port_.device} is found");
@@ -15,3 +18,8 @@ def find_port() -> (int, str):
         return (-1, port);
     return (0, port);
 
+def init_serial(serial: Serial):
+	serial.dtr: bool = False
+	serial.rts: bool = False
+	sleep(0.1)
+	serial.reset_input_buffer()
